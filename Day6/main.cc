a1a757563d16f12b8a04bea4b7d127d18da64cd2
Dannan Vazquez <dannanvazquez@gmail.com>
2022-12-06 01:28:28 -0800

    Day 6 Part 2 Solution
    Modified day 6's part 1 solution to be more flexible so it can solve both parts.

--- Day6/main.cc
+++ Day6/main.cc
@@ -14,17 +14,21 @@ int main() {
         istringstream iss(line);
         if (line.empty()) continue;
 
-        // Starting at position i, see if the first three characters(using j) have any duplicates ahead of them(using k) for a total of four characters being searched at a time.
-        for (int i = 0; i < line.size() - 3; i++) {
+        int distinctChars;
+        cout << "How many distinct characters are you looking for?\n";
+        cin >> distinctChars;
+
+        // Starting at position i, see if the first distinctChars - 1 characters(using j) have any duplicates ahead of them(using k) for a total of distinctChars being searched at a time.
+        for (int i = 0; i < line.size() - distinctChars - 1; i++) {
             bool isDuplicate = false;
-            for (int j = 0; j < 3; j++) {
-                for (int k = 1; k < 4 - j; k++) {
+            for (int j = 0; j < distinctChars - 1; j++) {
+                for (int k = 1; k < distinctChars - j; k++) {
                     if (line[i + j] == line[i + j + k]) {
                         isDuplicate = true;
                         i += j;  // Skip i to where the duplicate is since we already know starting at the characters in between the current starting point and duplicate will result in another failed loop.
                         break;
-                    } else if (j == 2) {
-                        processedChars = i + 4;
+                    } else if (j == distinctChars - 2) {
+                        processedChars = i + distinctChars;
                     }
                     
                 }
