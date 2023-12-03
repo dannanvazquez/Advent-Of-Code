d23d337696e2ae956d45781f758263f04d103b31
Dannan Vazquez <dannanvazquez@gmail.com>
2023-12-03 04:09:18 -0800

    2023: Day 3: Part 2

--- 2023/Day03/main.cpp
+++ 2023/Day03/main.cpp
@@ -5,8 +5,58 @@
 
 using namespace std;
 
+class Gear { 
+    public: 
+        int row, col;
+        vector<int> partNumbers;
+    
+        Gear(int row, int col, int partNumber) { 
+            this->row = row; 
+            this->col = col;
+            this->partNumbers.push_back(partNumber);
+        }
+
+        int GetGearRatio() {
+            if (partNumbers.size() == 2) {
+                return partNumbers[0] * partNumbers[1];
+            } else {
+                return 0;
+            }
+        }
+}; 
+
+class GearsVector { 
+    vector<Gear*> gears;
+  
+    public: 
+        void insertGear(int, int, int); 
+        int getGearRatioSum(); 
+};
+
+void GearsVector::insertGear(int row, int col, int partNumber) { 
+    for (auto gear : gears) {
+        if (gear->row == row && gear->col == col) {
+            gear->partNumbers.push_back(partNumber);
+            return;
+        }
+    }
+
+    Gear* newGear = new Gear(row, col, partNumber);
+    gears.push_back(newGear);
+} 
+  
+int GearsVector::getGearRatioSum() { 
+    int gearRatioSum = 0;
+
+    for (auto gear : gears) {
+        gearRatioSum += gear->GetGearRatio();
+    }
+
+    return gearRatioSum;
+} 
+
 int main() {
-    int totalSum = 0;
+    GearsVector gearsVector;
 
     string line;
     ifstream infile("input.txt");
@@ -24,59 +74,78 @@ int main() {
                 int digitSize = 1;
                 while (lines[i].size() > j + digitSize && isdigit(lines[i][j+digitSize])) digitSize++;
 
-                // Check for adjacent symbols.
-                bool isPartNumber = false;
-
                 bool hasSpaceLeft = j > 0;
                 bool hasSpaceRight = lines[i].size() > j + digitSize;
                 bool hasSpaceAbove = i > 0;
                 bool hasSpaceBelow = lines.size() > i + 1;
 
-                if (hasSpaceLeft && lines[i][j-1] != '.') {  // Check left of the number.
-                    isPartNumber = true;
-                } else if (hasSpaceRight && lines[i][j+digitSize] != '.') {  // Check right of the number.
-                    isPartNumber = true;
-                } else {
-                    if (hasSpaceAbove) {  // Check above the number.
-                        for (int k = (hasSpaceLeft ? -1 : 0); k < digitSize + (hasSpaceRight ? 1 : 0); k++) {
-                            char currentChar = lines[i-1][j+k];
-                            if (!isdigit(currentChar) && currentChar != '.') {
-                                isPartNumber = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (!isPartNumber && hasSpaceBelow) {  // Check below the number.
-                        for (int k = (hasSpaceLeft ? -1 : 0); k < digitSize + (hasSpaceRight ? 1 : 0); k++) {
-                            char currentChar = lines[i+1][j+k];
-                            if (!isdigit(currentChar) && currentChar != '.') {
-                                isPartNumber = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                // Finally, add to the sum if the entire number is a part number.
-                if (isPartNumber) {
+                // Check left of the number.
+                if (hasSpaceLeft && lines[i][j-1] == '*') {
                     string fullNumber;
-                    for (size_t k = 0; k < digitSize; k++) {
-                        fullNumber = fullNumber + lines[i][j+k];
-                    }
-                    
-                    totalSum += stoi(fullNumber);
+                    for (size_t k = 0; k < digitSize; k++) fullNumber = fullNumber + lines[i][j+k];
+                    gearsVector.insertGear(i, j-1, stoi(fullNumber));
+                    if (hasSpaceRight) {
+                        j += digitSize - 1;
+                        continue;
+                    } else break;
                 }
 
-                // Iterate the for loop past the numbers.
-                if (hasSpaceRight) {
+                // Check right of the number.
+                if (hasSpaceRight && lines[i][j+digitSize] == '*') {
+                    string fullNumber;
+                    for (size_t k = 0; k < digitSize; k++) fullNumber = fullNumber + lines[i][j+k];
+                    gearsVector.insertGear(i, j+digitSize, stoi(fullNumber));
                     j += digitSize - 1;
-                } else {
-                    break;
+                    continue;
+                }
+
+                // Check above the number.
+                if (hasSpaceAbove) {
+                    bool foundGear = false;
+                    for (int k = (hasSpaceLeft ? -1 : 0); k < digitSize + (hasSpaceRight ? 1 : 0); k++) {
+                        char currentChar = lines[i-1][j+k];
+                        if (currentChar == '*') {
+                            string fullNumber;
+                            for (size_t k = 0; k < digitSize; k++) fullNumber = fullNumber + lines[i][j+k];
+                            gearsVector.insertGear(i-1, j+k, stoi(fullNumber));
+                            foundGear = true;
+                            break;
+                        }
+                    }
+
+                    if (foundGear) {
+                        if (hasSpaceRight) {
+                            j += digitSize - 1;
+                            continue;
+                        } else break;
+                    }
+                }
+
+                // Check below the number.
+                if (hasSpaceBelow) {
+                    bool foundGear = false;
+                    for (int k = (hasSpaceLeft ? -1 : 0); k < digitSize + (hasSpaceRight ? 1 : 0); k++) {
+                        char currentChar = lines[i+1][j+k];
+                        if (currentChar == '*') {
+                            string fullNumber;
+                            for (size_t k = 0; k < digitSize; k++) fullNumber = fullNumber + lines[i][j+k];
+                            gearsVector.insertGear(i+1, j+k, stoi(fullNumber));
+                            foundGear = true;
+                            break;
+                        }
+                    }
+
+                    if (foundGear) {
+                        if (hasSpaceRight) {
+                            j += digitSize - 1;
+                            continue;
+                        } else break;
+                    }
                 }
             }
         }
     }
     
-
-    cout << "Total sum: " << totalSum << endl;
+    int gearRatioSum = gearsVector.getGearRatioSum();
+    cout << "Total sum: " << gearRatioSum << endl;
 }
